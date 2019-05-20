471cf2b0e8188c1537b5a0606a83fa2e5e5fa5e2
Kara O <karajo.odle@gmail.com>
2019-05-20 00:18:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find include?(cheese_types)
+  array.find do |x| 
+    x.include?(cheese_types.join(" ,"))
   end
 end
